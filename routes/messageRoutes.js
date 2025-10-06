e98d2c91ad4b511eb92882e8d13c80192792b212
ingabire3 <ingabirejosiane003@gmail.com>
2025-10-06 12:04:58 +0200

    .....

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -10,7 +10,10 @@ const { authMiddleware } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
+// Public route - for clients to send messages
 router.post('/', createMessage);
+
+// Protected routes - require authentication
 router.get('/', authMiddleware, getMessages);
 router.get('/client', authMiddleware, getClientMessages);
 router.put('/:id/reply', authMiddleware, updateMessageReply);
